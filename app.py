79610b4307fae9bbf99ecc23b069bd7bbfb9114a
Manu <manuel_villate@mckinsey.com>
2020-05-25 18:29:53 +0200

    CRUD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -15,8 +15,30 @@ movies = [
     }
 ]
 
-@app.route('/movies')
-def hello():
+
+@app.route('/', methods=['GET'])
+def show_movies():
     return jsonify(movies)
+
+
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    movie = request.get_json()
+    movies.append(movie)
+    return jsonify(movie), 200
+
+
+@app.route('/movies/<int:index>', methods=['PUT'])
+def update_movie(index):
+    movie = request.get_json()
+    movies[index] = movie
+    return jsonify(movies[index]), 200
+
+
+@app.route('/movies/<int:index>', methods=['DELETE'])
+def delete_movie(index):
+    movies.pop(index)
+    return 'None', 200
+
 
 app.run()
