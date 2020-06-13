9c65caab1089123f46084921720ac5eb2eca25eb
Manu <manuel_villate@mckinsey.com>
2020-06-13 23:21:43 +0200

    blueprint

--- app.py
+++ app.py
@@ -1,63 +1,16 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask
 from database.db import initialize_db
-from database.models import Movie
+from resources.movie import movies
 
 app = Flask(__name__)
 
-# movies = [
-#     {
-#         "name": "The Shawshank Redemption",
-#         "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
-#         "genres": ["Drama"]
-#     },
-#     {
-#        "name": "The Godfather ",
-#        "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
-#        "genres": ["Crime", "Drama"]
-#     }
-# ]
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
 }
 
 initialize_db(app)
-
-# @app.route('/movies')
-# def hello():
-#     return jsonify(movies)
-
-@app.route('/movies')
-def get_movies():
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
-
-
-@app.route('/movies', methods=['POST'])
-def add_movie():
-    body = request.get_json()
-    movie = Movie(**body).save()
-    id = movie.id
-    return {'id': str(id)}, 200
-
-
-@app.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    body = request.get_json()
-    Movie.objects.get(id=id).update(**body)
-    return '', 200
-
-
-@app.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(index):
-    movie = Movie.objects.get(id=id).delete()
-    return '', 200
-
-
-@app.route('/movies/<id>')
-def get_movie(id):
-    movies = Movie.objects.get(id=id).to_json()
-    return Response(movies, mimetype="application/json", status=200)
+app.register_blueprint(movies)
 
 
 app.run()
